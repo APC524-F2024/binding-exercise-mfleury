94cdd618f7288f383bdf59932185c9973c17dcb7
Maria Fleury <mfleury@princeton.edu>
2024-12-06 18:57:43 -0500

    binding exercise

--- src/_core.cpp
+++ src/_core.cpp
@@ -7,5 +7,9 @@
 namespace py = pybind11;
 
 PYBIND11_MODULE(_core, m) {
-  /// ... Fill me in!
-}
+  py::class_<HarmonicOscillator>(m, "HarmonicOscillator")
+      .def(py::init<>())
+      .def("f", &HarmonicOscillator::f)
+      .def("compute_step", &HarmonicOscillator::compute_step)
+      .def("integrate", &HarmonicOscillator::integrate);
+      }
